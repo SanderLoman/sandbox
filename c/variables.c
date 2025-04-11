53df5ebe08a1ddbe9a275b08d3721d458b72c340
SanderLoman <sanderfeitsma13@gmail.com>
2025-04-12 00:19:55 +0200

    Add functions demonstration with basic operations and memory address outputs

--- c/variables.c
+++ c/variables.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
+int main()
+{
     bool a = true;
     char b = 'a';
     int c = 1;
     float d = 1.1;
 
     // This can represent binary
-    signed char e = -5; // Currently says: 11111011
-    unsigned char e = 5; // Currently says: 00000101
+    // signed char e = -5; // Currently says: 11111011
+    // unsigned char e = 5; // Currently says: 00000101
 
-    printf("This signed char in binary says: %d\n", e);
+    printf("Output: %d\n%c\n%d\n%f\n", a, b, c, d);
 
     return 0;
 }
