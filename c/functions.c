dcad022b6eea5166d5b2d0a228ae0442fe7655bd
SanderLoman <sanderfeitsma13@gmail.com>
2025-04-19 20:31:27 +0200

    Format change

--- c/functions.c
+++ c/functions.c
@@ -17,15 +17,15 @@ void addTwo(int a, int b)
 
 int main(void)
 {
-    int myAge = 21;     // An int variable
-    int* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
-    
+    int myAge = 21;    // An int variable
+    int *ptr = &myAge; // A pointer variable, with the name ptr, that stores the address of myAge
+
     // Output the value of myAge (21)
     printf("%d\n", myAge);
-    
+
     // Output the memory address of myAge (0x7ffe5367e044)
     printf("%p\n", &myAge);
-    
+
     // Output the memory address of myAge with the pointer (0x7ffe5367e044)
     printf("%p\n", ptr);
 
