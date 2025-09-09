cb89fecfa0e2b83a8eb4a3fa6dc46f3815739cd5
Sander Loman <sander.loman@hva.nl>
2025-09-10 00:54:50 +0200

    stuggling

--- c/small_projects/temperature_conversion.cpp
+++ c/small_projects/temperature_conversion.cpp
@@ -1,6 +1,19 @@
 #include <cstdio>
+#include <cstring>
 
-void main()
-{
-    scanf();
+typedef enum Type {
+    CELCIUS,
+    FAHRENHEIT
+} type;
+
+int main(void) {
+    type unit;
+    char choice;
+
+    printf("Choose a unit to convert from: CELCIUS (c) or FAHRENHEIT (f)\n");
+
+    scanf("%c", &choice);
+    printf("%c\n", choice);
+
+    return 0;
 }
